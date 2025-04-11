3a2dfc39127f4fcc7fdebb271abcbca0c8155f2f
3Nina <167197662+3Nina@users.noreply.github.com>
2025-04-11 10:27:20 -0300

    Simulacro heladería completo

---
+++ Assets/Scripts/HeladeriaLosTresGustos.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeladeriaLosTresGustos : MonoBehaviour
+{
+    public string gusto;
+    public int cantHeladoGramos;
+
+    float promocionFrutilla = 0.9f;
+    float precioHelado;
+    float precioPorKilo = 1250;
+ 
+    
+
+    void Start()
+    {
+     
+        if (cantHeladoGramos < 250 || cantHeladoGramos > 3000)
+        {
+            Debug.Log("Cantidad de helado no válida");
+            return;
+        }
+        if (gusto != "CHO" && gusto != "FRU" && gusto != "DDL")
+        {
+            Debug.Log("Código no válido");
+            return;
+        }
+
+         precioHelado = cantHeladoGramos * precioPorKilo / 1000 ;
+
+         if (gusto == "FRU")
+         {
+            precioHelado = cantHeladoGramos * promocionFrutilla;
+         }
+
+
+        Debug.Log("Debe abonar $ " + precioHelado);
+
+    }
+
+
+
+    void Update()
+    {
+        
+    }
+}
